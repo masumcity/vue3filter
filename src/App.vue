1667eaf18d15afad57d1efa62c17529f5701dcda
masumcity <riaz.masum@comcitybd.com>
2023-02-14 15:44:07 +0600

    new number

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,22 @@
 <template>
   <div class="container mx-auto">
     <h1 class="text-red-600 text-4xl p-6">IP Phone Extensions</h1>
+    <Card />
 
     <form class="flex items-center mx-8">
       <label for="simple-search" class="sr-only">Search</label>
       <div class="relative w-full">
-        <div
-          class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
-        >
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5 text-gray-500 dark:text-gray-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-              clip-rule="evenodd"
-            ></path>
+              clip-rule="evenodd"></path>
           </svg>
         </div>
-        <input
-          type="search"
-          id="simple-search"
+        <input type="search" id="simple-search"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search"
-          v-model="search"
-          required
-        />
+          placeholder="Search" v-model="search" required />
       </div>
     </form>
 
@@ -42,46 +29,28 @@
             <table class="min-w-full">
               <thead class="bg-white border-b">
                 <tr>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
+                  <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                     #
                   </th>
 
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
+                  <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                     Name
                   </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
+                  <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                     Extension
                   </th>
                 </tr>
               </thead>
               <tbody>
-                <tr
-                  v-for="(list, index) in filtereds"
-                  :key="index"
-                  class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
-                >
-                  <td
-                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
-                  >
+                <tr v-for="(list, index) in filtereds" :key="index"
+                  class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                     {{ index + 1 }}
                   </td>
-                  <td
-                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
-                  >
+                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                     {{ list.name }}
                   </td>
-                  <td
-                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                  >
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                     {{ list.ext }}
                   </td>
                 </tr>
@@ -96,12 +65,17 @@
 
 <script>
 import lists from "../src/data/extension.json";
+import Card from '../src/components/Card.vue';
 export default {
   data() {
     return {
       jsonlists: [],
       search: "",
     };
+  },
+
+  components: {
+    Card
   },
 
   mounted() {
